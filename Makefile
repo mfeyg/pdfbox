5a211ea44c7bf5758da687d414f71c9001e49499
Andrew Johnson <andrewbrettjohnson@gmail.com>
2015-07-20 01:24:20 +0000

    mvp

--- Makefile
+++ Makefile
@@ -1,7 +1,7 @@
 CP = commons-logging-1.2.jar:pdfbox-1.8.9.jar:.
 
 all:
-	javac -classpath $(CP) org/apache/pdfbox/examples/pdmodel/*.java
+	javac -classpath $(CP) -Xlint:unchecked *.java
 
 run:
-	java -classpath $(CP) org.apache.pdfbox.examples.pdmodel.CreateBookmarks in.pdf out.pdf
+	java -classpath $(CP) TOC toc.txt in.pdf out.pdf
--- TOC.java
+++ TOC.java
@@ -3,55 +3,76 @@ import org.apache.pdfbox.pdmodel.PDPage;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.destination.PDPageFitWidthDestination;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDDocumentOutline;
 import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineItem;
+import org.apache.pdfbox.pdmodel.interactive.documentnavigation.outline.PDOutlineNode;
 
 import java.util.Stack;
 import java.util.List;
+import java.nio.file.Path;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.io.IOException;
+import java.nio.charset.Charset;
 
 class TOC {
-    public static void main(String[] args) throws IOException {
-        String filename = args[0];
-        System.out.println(filename);
-        Stack<int> indent_levels = new Stack<int>();
-        Stack<PDOutlineItem> lineage = new Stack<PDOutlineItem>();
-        PDOutlineItem outline = new PDOutlineItem();
-        indent_levels.push(0);
+    public static void main(String[] args) throws Exception {
+        Path tocPath = Paths.get(args[0]);
+        PDDocument document = PDDocument.load( args[1] );
+        PDDocumentOutline outline =  new PDDocumentOutline();
+        document.getDocumentCatalog().setDocumentOutline( outline );
+        List pages = document.getDocumentCatalog().getAllPages();
+        Stack<Integer> indent_levels = new Stack<Integer>();
+        Stack<PDOutlineNode> lineage = new Stack<PDOutlineNode>();
+        indent_levels.push(-1);
         lineage.push(outline);
-        List<String> lines = Files.readAllLines(Paths.get(filename));
+        List<String> lines = Files.readAllLines(tocPath,
+                                                Charset.defaultCharset());
         for (String line : lines) {
-            int indentation = line.getIndentation();
-            if (indentation > indent_levels.peek()) {
-                indent_levels.push(indentation);
-                lineage.peek().appendChild();
-                //child
+            Entry entry = parseEntry(line);
+            PDOutlineItem item = new PDOutlineItem();
+            item.setTitle(entry.title);
+            PDPageFitWidthDestination dest = new PDPageFitWidthDestination();
+            dest.setPage( (PDPage) pages.get(entry.pageNum-1) );
+            item.setDestination(dest);
+            while (indent_levels.peek() >= entry.indent) {
+                indent_levels.pop();
+                lineage.pop();
             }
-            else if (indentation == stack.peek()) {
-                // sibling
-            }
-            else {
-
-            }
+            lineage.peek().appendChild(item);
+            lineage.push(item);
+            indent_levels.push(entry.indent);
         }
-    }
-
-    static String getTitle(String line) {
         
-    }
+        outline.openNode();
 
-    static int getIndentation(String line) {
-        int result = 0;
+        document.save( args[2] );
+    }
+    
+    static class Entry {
+        public int indent;
+        public String title;
+        public int pageNum;
+    }
+    
+    static Entry parseEntry(String line) {
+        Entry entry = new Entry();
         int i = 0;
+        // read indentation level
         while (true) {
             if (line.charAt(i) == '\t')
-                result += 4;
+                entry.indent += 4;
             else if (line.charAt(i) == ' ')
-                ++result;
+                ++entry.indent;
             else
                 break;
             ++i;
         }
-        return result;
+        int titleStart = i;
+        i = line.length() - 1;
+        while (line.charAt(i) != '\t' && line.charAt(i) != ' ') --i;
+        ++i;
+        entry.pageNum = Integer.parseInt(line.substring(i));
+        while (line.charAt(i) == '\t' || line.charAt(i) == ' ') --i;
+        entry.title = line.substring(titleStart, i);
+        return entry;
     }
 }
